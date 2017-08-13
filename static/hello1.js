419dded94d9e04a351fba647be061b1dde8136b3
liunix <liuyifan.com@gmail.com>
2017-08-13 15:05:40 +0800

    tidy

--- static/hello1.js
+++ static/hello1.js
@@ -1,7 +1,7 @@
 /** 进行宽高计算、D3伸缩计算 */
-var margin = { top: 20, right: 20, bottom: 30, left: 50 },
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = { top: 20, right: 20, bottom: 30, left: 50 }
+var width = 960 - margin.left - margin.right,
+var height = 500 - margin.top - margin.bottom;
 
 var parseDate = d3.timeParse("%d-%b-%y"); //函数式引用
 
